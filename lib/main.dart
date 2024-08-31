a5686c8630a6effa50f8b2bc10bdcacf5816639d
aeropriest <ashok.jaiswal@gmail.com>
2024-08-31 22:23:06 +0800

    * Added the tts and gemini

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 late List<CameraDescription> cameras;
 
@@ -42,18 +44,25 @@ class _CameraScreenState extends State<CameraScreen> {
   late CameraController controller;
   late Future<void> initializeControllerFuture;
   late stt.SpeechToText _speechToText;
+  late FlutterTts tts; // Initialize Flutter TTS
+
   bool _speechEnabled = false;
   String _lastWords = '';
 
   @override
   void initState() {
     super.initState();
+    Gemini.init(
+        apiKey: 'AIzaSyB9sU_QRwE8hYt1lkh6vK0xBkJ5M6Tgbx8',
+        enableDebugging: true);
+
     controller = CameraController(
       widget.camera,
       ResolutionPreset.max,
     );
     initializeControllerFuture = controller.initialize();
     _initSpeech();
+    tts = FlutterTts(); // Initialize TTS
   }
 
   Future<void> _initSpeech() async {
@@ -65,17 +74,54 @@ class _CameraScreenState extends State<CameraScreen> {
     setState(() {});
   }
 
+  Future<void> _getGeminiResponse(question) async {
+    final gemini = Gemini.instance;
+
+    var result = "";
+    // gemini.textAndImage(
+    //   text: _lastWords,
+    //   images: [selectedImage.readAsBytesSync()],
+    // ).then((value) {
+    //   log('Got the response...');
+
+    //   // Check if the response is not null and has output
+    //   if (value != null && value.output != null) {
+    //     results = value.output!;
+    //   }
+    // };
+
+    var prompt =
+        "Pretend you are talking to a 4-8 years old child, answer the following question in simple words, keep the conversation playful and engaging by asking a leading question " +
+            question;
+    print(prompt);
+    gemini.text(prompt).then((value) {
+      String results = "Show results here...";
+      results = value!.output!;
+      _speak(results);
+    }).catchError((e) => print(e));
+  }
+
+  Future<void> _speak(text) async {
+    await tts.setLanguage("en-US");
+    await tts.setPitch(1.0);
+    await tts.speak(text);
+  }
+
   void _startListening() {
     _speechToText.listen(
       onResult: (val) {
         setState(() {
           print('set here 3');
-          _lastWords = val.recognizedWords.split('\n').first;
+          _lastWords = val.recognizedWords;
           print(_lastWords.length);
         });
+
+        // Call _speak here to convert the recognized words to speech
+        // _speak(_lastWords); // Example text
+        _getGeminiResponse(_lastWords);
       },
+      // Uncomment this if you want to restart listening when done
       // onDone: () {
-      //   // Restart listening when done
       //   _startListening();
       // },
     );
@@ -140,16 +186,18 @@ class _CameraScreenState extends State<CameraScreen> {
                   ),
                 ),
                 Positioned(
-                  bottom: 100, // Adjust as needed
+                  bottom: 0, // Adjust as needed
                   left: 0,
                   right: 0,
                   child: Container(
                     padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     color: Colors.black54,
                     child: Text(
-                      _lastWords.length > 30
-                          ? _lastWords.substring(0, _lastWords.length - 30)
-                          : _lastWords,
+                      // _lastWords.length > 60
+                      //     ? _lastWords.substring(
+                      //         _lastWords.length - 60, _lastWords.length)
+                      //     : _lastWords,
+                      _lastWords,
                       style: const TextStyle(
                         color: Colors.white,
                         fontSize: 24,
